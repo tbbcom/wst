68102370fde017a3566b642d251381fd1448a82f
Bukit Besi Junior <69172989+bukitbesi@users.noreply.github.com>
2025-10-04 07:20:56 +0800

    Update sticker-maker.js

--- whatsapp-sticker-maker/sticker-maker.js
+++ whatsapp-sticker-maker/sticker-maker.js
@@ -473,7 +473,7 @@
         // Initialize particles animation
         function createParticles() {
             const container = document.createElement('div');
-            container.className = 'particle-icontainer';
+            container.className = 'particle-container';
             document.body.appendChild(container);
             
             for (let i = 0; i < 20; i++) {
